dba1a85081eaf7eba5b8c50e0655582f1062c58e
paulo.santosDell <afonso.mea@gmail.com>
2022-05-20 08:17:00 -0300

    Cria Bloc e implementa na aplicação

--- lib/pages/weather_search_page.dart
+++ lib/pages/weather_search_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_cubit_bloc_tutorial/cubit/weather_cubit.dart';
+import 'package:flutter_cubit_bloc_tutorial/bloc/weather_bloc.dart';
 import 'package:flutter_cubit_bloc_tutorial/data/model/weather.dart';
 
 class WeatherSearchPage extends StatefulWidget {
@@ -14,12 +14,14 @@ class _WeatherSearchPageState extends State<WeatherSearchPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Weather Search"),
+        centerTitle: true,
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 16),
         alignment: Alignment.center,
         // TODO: Implement with cubit
-        child: BlocConsumer<WeatherCubit, WeatherState>(
+        //child: BlocConsumer<WeatherCubit, WeatherState>(
+        child: BlocConsumer<WeatherBloc, WeatherState>(
           listener: (context, state){
             if (state is WeatherError){
               ScaffoldMessenger.of(context).showSnackBar(
@@ -97,8 +99,10 @@ class CityInputField extends StatelessWidget {
   }
 
   void submitCityName(BuildContext context, String cityName) {
-    final weatherCubit = context.read<WeatherCubit>();
+    //final weatherCubit = context.read<WeatherCubit>();
+    final weatherBloc = context.read<WeatherBloc>();
 
-    weatherCubit.getWeather(cityName);
+    //weatherCubit.getWeather(cityName);
+    weatherBloc.add(GetWeather(cityName));
   }
 }
